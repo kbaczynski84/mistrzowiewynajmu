d8aa13daa9596591e342859477939065e5800c5e
kbaczynski84 <k.baczynski84@gmail.com>
2018-04-20 21:54:25 +0200

    second angular service

---
+++ MistrzowieWynajmu1/MistrzowieWynajmu1/ClientApp/app/components/property/services/PropertiesService.ts
@@ -0,0 +1,26 @@
+﻿import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Property } from '../../../models/property';
+import { PropertiesBackendService } from '../../../services/properties-backend.service';
+
+@Injectable()
+export class PropertiesService {
+
+    constructor(private propertiesBackendService: PropertiesBackendService) { }
+
+    addProperty(newProperty: Property): Observable<number> {
+        return this.propertiesBackendService.addProperty(newProperty);
+    }
+    getProperty(id: number): Observable<Property> {
+        return this.propertiesBackendService.getProperty(id);
+    }
+    getPropertie(): Observable<Property[]> {
+        return this.propertiesBackendService.getProperties();
+    }
+    updateProperty(propertyToUpdate: Property): Observable<number> {
+        return this.propertiesBackendService.updateProperty(propertyToUpdate);
+    }
+    deletePropert(id: number): Observable<number> {
+        return this.propertiesBackendService.deleteProperty(id);
+    }
+}
